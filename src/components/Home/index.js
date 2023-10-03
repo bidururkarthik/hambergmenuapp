c8f951abc1a200190bbddc3223a9ad32a7935de6
bidururkarthik <bidururkarthik1212@gmail.com>
2023-10-03 08:35:14 +0000

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+const Home = () => (
+  <div>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+      alt="home"
+    />
+  </div>
+)
+
+export default Home
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+const NotFound = () => (
+  <div>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-img.png"
+      alt="not found"
+    />
+  </div>
+)
+
+export default NotFound
